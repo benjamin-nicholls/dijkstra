6dd6b8d1e31274fea850cafe594af55944755585
Benjamin <benjaminpnicholls@gmail.com>
2022-05-18 22:19:54 +0100

    Finished A* implementation. Added gHeuristic. Added comments.

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
 #include <list>
 #include <cmath>
 #include "bots.h"
 
+int gHeuristic = 0;  // Global variable to choose heuristic.
 
 struct nodeCostStruct {
 	int x = 0;
@@ -10,14 +10,9 @@ struct nodeCostStruct {
 	float cost = 1000000.0f;
 };
 
-
-cDijkstra::cDijkstra() {
-	completed = false;
-};
-
-cDijkstra::~cDijkstra() {};
-
-
+//======================================================================================
+// Dijkstras
+//======================================================================================
 bool cDijkstra::getInPath(int x, int y) const {
 	return graph[x][y].inPath;
 }
@@ -27,13 +22,12 @@ bool cDijkstra::getClosed(int x, int y) const {
 }
 
 void cDijkstra::ChooseNextGridPosition() {
-	// no implementation yet
+	// no implementation 
 }
 
 void cDijkstra::Build(cBotBase& bot) {
 	graph[bot.PositionX()][bot.PositionY()].cost = 0;
-	delete graph;
-	graphNode graph[GRIDWIDTH][GRIDHEIGHT];
+
 	nodeCostStruct node;
 	nodeCostStruct neighbourDeltas[8]
 		= { {-1, -1, 1.4f},
@@ -80,7 +74,7 @@ void cDijkstra::Build(cBotBase& bot) {
 				graph[tmpX][tmpY].cost = newCost;
 				graph[tmpX][tmpY].linkX = node.x;
 				graph[tmpX][tmpY].linkY = node.y;
-				std::cout << "lowest cost node: x=" << node.x << ", y=" << node.y << std::endl;
+				//std::cout << "lowest cost node: x=" << node.x << ", y=" << node.y << std::endl;
 			}
 		}
 	}
@@ -104,18 +98,22 @@ void cDijkstra::Build(cBotBase& bot) {
 }
 
 
-
-cAStar::cAStar() {
-	completed = false;
-};
-
-cAStar::~cAStar() {};
-
+//======================================================================================
+// A STAR IMPLEMENTATION
+//======================================================================================
 
 void cAStar::Build(cBotBase& bot) {
+	// Reset values.
+	for (int x = 0; x < GRIDWIDTH; x++) {
+		for (int y = 0; y < GRIDHEIGHT; y++) {
+			graph[x][y].reset();
+		}
+	}
+	// Set bot position as starting position.
 	graph[bot.PositionX()][bot.PositionY()].cost = 0;
-
+	// Stores lowest cost node coordinates and cost.
 	nodeCostStruct node;
+	// X and Y offsets for neighbours.
 	nodeCostStruct neighbourDeltas[8]
 		= { {-1, -1, 1.4f},
 			{0, -1, 1.0f},
@@ -124,39 +122,47 @@ void cAStar::Build(cBotBase& bot) {
 			{1, 1, 1.4f},
 			{0, 1, 1.0f},
 			{-1, 1, 1.4f},
-			{-1, 0, 1.0f} };  // Starting top left, goes clockwise.
-	bool flag = false;
-
+			{-1, 0, 1.0f} };  
+	
+	// Calculate heuristic value for the entire map.
 	for (int x = 0; x < GRIDWIDTH; x++) {
 		for (int y = 0; y < GRIDHEIGHT; y++) {
-			int dx = gTarget.PositionX() - x;
-			int dy = gTarget.PositionY() - y;
-			// Total difference in X + total difference in Y.
-			int manhattanDistance = abs(dx) + abs(dy);
-			// D1*(dx + dy) + (D2 - 2 * D1) * Min(dx , dy). In our case: D1 = 1, D2 = 1.4
-			int smallest = dy;
-			if (dx < dy) { smallest = dx;  }
-			int diagonalDistance = (dx + dy) + 0.6*smallest;
-			// Sqrt((difference in X)2 + (difference in Y)2)
-			//int euclideanDistance = sqrt( pow(dx,2) + pow(dy,2) );
+			int dx = abs(gTarget.PositionX() - x);
+			int dy = abs(gTarget.PositionY() - y);
 
-			graph[x][y].heuristic = manhattanDistance;
+			float  heuristic = 0.0f;
+			if (gHeuristic == 2) {  
+				// Diagonal Distance
+				// D1*(dx + dy) + (D2 - 2 * D1) * Min(dx , dy). In our case: D1 = 1, D2 = 1.4
+				heuristic = (dx + dy) - 0.6 * std::min(dx, dy);
+			} else if (gHeuristic == 3) {  
+				// Euclidean Distance
+				// Sqrt((difference in X)2 + (difference in Y)2)
+				heuristic = sqrt(pow(dx, 2) + pow(dy, 2));
+			} else { 
+				// Manhatten Disance
+				// Total difference in X + total difference in Y.
+				heuristic = dx + dy;
+			}
+			graph[x][y].heuristic = heuristic;
 		}
 	}
 
-
+	// 1. Find the grid location with the lowest cost, that is not closed, and is not blocked on the map layout.
+	bool flag = false;
 	while (!flag) {
 		node.cost = 1000000.0f;
 		for (int x = 0; x < GRIDWIDTH; x++) {
 			for (int y = 0; y < GRIDHEIGHT; y++) {
-				// 1. Find the grid location with the lowest cost, that is not closed, and is not blocked on the map layout.
-				if (!gLevel.isValid(x, y)) { continue; }  // Has to be a valid location.
-				if (graph[x][y].closed == true) { continue; }  // Has to be an open location.
+				
+				// Has to be a valid location.
+				if (!gLevel.isValid(x, y)) { continue; }  
+				// Has to be an open location.
+				if (graph[x][y].closed == true) { continue; }  
 				if (graph[x][y].cost + graph[x][y].heuristic < node.cost) {
 					node.x = x;
 					node.y = y;
 					node.cost = graph[x][y].cost + graph[x][y].heuristic;
-					std::cout << "lowest cost node: x=" << node.x << ", y=" << node.y << ", cost=" << node.cost << ", heuristic=" << graph[x][y].heuristic << std::endl;
 					//rootNode = (node){.x = y, .y = x, .cost = cost};  // Not running C99?
 				}
 			}
@@ -167,7 +173,7 @@ void cAStar::Build(cBotBase& bot) {
 		if ((node.x == gTarget.PositionX()) && (node.y == gTarget.PositionY())) {
 			flag = true;
 		}
-		// 3. Calculate new cost for 8 neighbours. parent+neighbour 1 and 1.4 diag
+		// 3. Calculate new cost for 8 neighbours.
 		for (int index = 0; index < sizeof(neighbourDeltas) / sizeof(*neighbourDeltas); index++) {
 			int tmpX = node.x + neighbourDeltas[index].x;
 			int tmpY = node.y + neighbourDeltas[index].y;
@@ -184,6 +190,7 @@ void cAStar::Build(cBotBase& bot) {
 		}
 	}
 
+	// Build path.
 	bool done = false;  // Set to true when we are back at the bot position.
 	int nextClosedX = gTarget.PositionX();  // Start of path.
 	int nextClosedY = gTarget.PositionY();  // Start of path.
@@ -191,8 +198,7 @@ void cAStar::Build(cBotBase& bot) {
 	nextClosedCoords.x = nextClosedX;
 	nextClosedCoords.y = nextClosedY;
 	pathVec.push_back(nextClosedCoords);
-	while (!done)
-	{
+	while (!done) {
 		graph[nextClosedX][nextClosedY].inPath = true;
 		int tmpX = nextClosedX;
 		int tmpY = nextClosedY;
@@ -205,7 +211,7 @@ void cAStar::Build(cBotBase& bot) {
 			done = true;
 		}
 	}
-	pathCounter = pathVec.size() - 2 ;
+	pathCounter = pathVec.size() - 2;  // -2 deals with space at end of vector.
 	completed = true;
 }
 
